9547953c8a15ee6a890b0395e79cd6be899081a9
beingcooper <sachin.sharma0728@gmail.com>
2015-11-07 18:57:23 +0530

    language filter

--- lang_filter.py
+++ lang_filter.py
@@ -1,13 +1,16 @@
+#To copy all the English language tweets in a new JSON file
 
 import json
 import fileinput
 
 
-outfile = open("output","w")
+outfile = open("output_file","w")
 
 #Paste only English tweets in the output file  
-for line in fileinput.input("input"):
-    tweet = json.loads(line)
-    if tweet["lang"] == 'en':
-        json.dump(tweet,outfile)
+if __name__ == "__main__":
+	for line in fileinput.input("input_file"):
+	    tweet = json.loads(line)
+	    if tweet["lang"] == 'en':
+	        twit = json.dumps(tweet)
+	        outfile.write(twit + '\n')
 
