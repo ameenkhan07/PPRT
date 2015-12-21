a06d77d993f1563f12b729960902539955f1996a
beingcooper <sachin.sharma0728@gmail.com>
2015-12-21 22:56:22 +0530

    location filter

---
+++ Geo.py
@@ -0,0 +1,13 @@
+
+from __future__ import print_function
+
+import json
+import fileinput
+
+for line in fileinput.input():
+    tweet = json.loads(line)
+    if 'retweeted_status' in tweet:
+        if tweet['retweeted_status']['geo']:
+            print(json.dumps(tweet))
+    elif tweet['geo']:
+        print(json.dumps(tweet))
